923cf027a23d091de011925456dcdc90c1dc405c
fuadhasbullah <70281865+fuadhasbullah@users.noreply.github.com>
2024-06-23 15:29:12 +0700

    Add files via upload

---
+++ adsterra.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '53a824ac5dd5ce6e301a61add9f3930e',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/53a824ac5dd5ce6e301a61add9f3930e/invoke.js"></script>
